076ab12f0bea02c8cb812f536addb2c4c21128fb
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-10 17:41:18 +0200

    layout header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,14 @@
 
 @section('page-title', 'Home Page')
 
+
+@section('menu')
+    @foreach($headerSections as $headerSection)
+        <li><a href="#">{{$headerSection}}</a></li>
+    @endforeach
+@endsection
+
+
 @section('content')
     <h1>io sono il content</h1>
 @endsection
